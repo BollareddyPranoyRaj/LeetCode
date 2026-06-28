f5feccfa31c049d0b7d95efaabba7de623cc855c
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2026-06-28 08:36:36 +0530

    Time: 7 ms (66.65%), Space: 22.3 MB (52.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -3,22 +3,23 @@ class Solution:
         c1=c2=0
         cand1=cand2=None
         for i in nums:
-            if cand1==i:
-                c1+=1
-            elif cand2==i:
-                c2+=1
-            elif c1==0:
-                cand1=i
-                c1=1
-            elif c2==0:
-                cand2=i
-                c2=1
+            if cand1 == i:
+                c1 += 1
+            elif cand2 == i:
+                c2 += 1
+            elif c1 == 0:
+                cand1 = i
+                c1 = 1
+            elif c2 == 0:
+                cand2 = i
+                c2 = 1
             else:
-                c1-=1
-                c2-=1
+                c1 -= 1
+                c2 -= 1
         k=[]
-        if nums.count(cand1)>len(nums)//3:
+        if nums.count(cand1) > len(nums)//3:
             k.append(cand1)
-        if nums.count(cand2)>len(nums)//3:
+        if nums.count(cand2) > len(nums)//3:
             k.append(cand2)
         return k
+        
