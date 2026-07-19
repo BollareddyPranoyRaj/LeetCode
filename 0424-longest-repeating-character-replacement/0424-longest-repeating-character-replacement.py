3c5da7f1abe71a93c202ffed39b7cdee39587f11
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2026-07-19 13:25:42 +0530

    Time: 75 ms (89.25%), Space: 19.7 MB (50.13%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,9 +3,11 @@ class Solution:
         dct={}
         i=0
         res=0
+        maxFreq=0
         for j in range(len(s)):
             dct[s[j]]=dct.get(s[j],0)+1
-            if (j-i+1)-max(dct.values())<=k:
+            maxFreq = max(maxFreq, dct[s[j]])
+            if (j-i+1)-maxFreq<=k:
                 res=max(res,(j-i+1))
             else:
                 dct[s[i]]-=1
