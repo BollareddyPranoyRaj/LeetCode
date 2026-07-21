51bdf11571406e060bc85e284c360e79125453b1
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2026-07-22 02:25:00 +0530

    Time: 11 ms (33.24%), Space: 21 MB (85.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hsh={}
-        for x in nums:
-            if x not in hsh:
-                hsh[x]=0
-            hsh[x]+=1
-        for x in hsh:
-            if hsh[x]>len(nums)//2:
-                return x 
+        n=len(nums)
+        for i in nums:
+            hsh[i]=hsh.get(i,0)+1
+            if hsh[i] > (n//2):
+                return i
         
