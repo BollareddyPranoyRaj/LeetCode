c8eb1244e00026898fe568fc365d8bfe8c48d8b0
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2026-07-24 11:03:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (54.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x,n)
+        ans=1
+        if n==0:
+            return 1
+        if n<0:
+            x=1/x
+            n=abs(n)
+        while n>0:
+            if n%2!=0:
+                ans*=x
+            x*=x
+            n//=2
+        return ans
+
+        
 
 
         
